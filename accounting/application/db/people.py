479d8b2d6e1a62607583f6d23fc019876cf93f52
LikeAproger <progforsoul@gmail.com>
2022-08-01 19:09:46 +0300

    initial commit

---
+++ accounting/application/db/people.py
@@ -0,0 +1,7 @@
+import datetime
+
+
+def get_employees():
+    print('getting employees started')
+    print(datetime.date.today())
+    print('getting employees finished\n')
---
+++ accounting/application/salary.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+
+def calculate_salary():
+    print('calculation salary started')
+    print(date.today())
+    print('calculation salary finished\n')
